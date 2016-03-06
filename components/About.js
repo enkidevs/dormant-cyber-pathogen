f56751bfc3b370ee441aa283deb89cbed5c101ec
Mathieu Dutour <mathieu@dutour.me>
2016-03-06 14:41:24 +0000

    third commit

--- components/About.js
+++ components/About.js
@@ -33,49 +33,77 @@ class About extends Component {
             width: this.state.size,
             height: this.state.size,
             borderRadius: this.state.radius}]}>
-          <Text style={styles.aboutButton}
-            onPress={this.showAbout.bind(this)}>?</Text>
+          <Text style={styles.aboutButton} onPress={this.showAbout.bind(this)}>?</Text>
         </Animated.View>
-        {this.state.showingAbout &&
-          <Animated.View
-            style={[styles.textView, {opacity: this.state.opacity}]}>
-            <Text style={styles.text}>bla bla bla</Text>
-          </Animated.View>
-        }
+        <Animated.View
+          pointerEvents={this.state.showingAbout ? 'auto' : 'none'}
+          style={[styles.textView, {width, height}, {opacity: this.state.opacity}]}>
+          <Text style={styles.closeButton} onPress={this.hideAbout.bind(this)}>x</Text>
+          <Text style={styles.text}>bla bla bla</Text>
+        </Animated.View>
       </View>
     )
   }
 
   showAbout () {
-    if (!this.state.showingAbout) {
-      const {height, width} = Dimensions.get('window')
-      const change = 2 * Math.max(height, width) + 40
-      const config = {
-        easing: Easing.quad,
-        duration: 200
-      }
-      Animated.timing(
-        this.state.size,
-        Object.assign({}, config, {toValue: change}),
-      ).start()
-      Animated.timing(
-        this.state.radius,
-        Object.assign({}, config, {toValue: change / 2}),
-      ).start()
-      Animated.timing(
-        this.state.x,
-        Object.assign({}, config, {toValue: -change / 2 + 10}),
-      ).start()
-      Animated.timing(
-        this.state.y,
-        Object.assign({}, config, {toValue: -change / 2 + 30}),
-      ).start()
-      Animated.timing(
-        this.state.opacity,
-        {toValue: 1, delay: 200, duration: 200},
-      ).start()
-      this.setState({showingAbout: true})
+    if (this.state.showingAbout) { return }
+    const {height, width} = Dimensions.get('window')
+    const change = 2 * Math.max(height, width) + 40
+    const config = {
+      easing: Easing.quad,
+      duration: 200
     }
+    Animated.timing(
+      this.state.size,
+      Object.assign({}, config, {toValue: change}),
+    ).start()
+    Animated.timing(
+      this.state.radius,
+      Object.assign({}, config, {toValue: change / 2}),
+    ).start()
+    Animated.timing(
+      this.state.x,
+      Object.assign({}, config, {toValue: -change / 2 + 10}),
+    ).start()
+    Animated.timing(
+      this.state.y,
+      Object.assign({}, config, {toValue: -change / 2 + 30}),
+    ).start()
+    Animated.timing(
+      this.state.opacity,
+      {toValue: 1, delay: 200, duration: 200},
+    ).start()
+    this.setState({showingAbout: true})
+  }
+
+  hideAbout () {
+    if (!this.state.showingAbout) { return }
+    const config = {
+      easing: Easing.quad,
+      delay: 200,
+      duration: 200
+    }
+    Animated.timing(
+      this.state.size,
+      Object.assign({}, config, {toValue: 26}),
+    ).start()
+    Animated.timing(
+      this.state.radius,
+      Object.assign({}, config, {toValue: 13}),
+    ).start()
+    Animated.timing(
+      this.state.x,
+      Object.assign({}, config, {toValue: 10}),
+    ).start()
+    Animated.timing(
+      this.state.y,
+      Object.assign({}, config, {toValue: 30}),
+    ).start()
+    Animated.timing(
+      this.state.opacity,
+      {toValue: 0, duration: 200},
+    ).start()
+    this.setState({showingAbout: false})
   }
 }
 
@@ -96,6 +124,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#B85667'
   },
   text: {
+    color: '#F2F3CB'
+  },
+  closeButton: {
+    position: 'absolute',
+    top: 30,
+    right: 10,
     color: '#F2F3CB'
   }
 })
